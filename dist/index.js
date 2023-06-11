4fd34ef82255925271e6f841c652e44bf287017f
kk <turnitsimple001@gmail.com>
2023-06-11 16:10:40 +0530

    logged

--- dist/index.js
+++ dist/index.js
@@ -19434,6 +19434,8 @@ function getFirebaseConfig(jsonStr) {
     // if (jsonStr.trim() == "") core.setFailed("FireBase Config is Empty");
     const json = JSON.parse(JSON.stringify(jsonStr));
     console.log(json);
+    console.log(Object.keys(json).map((e) => console.log(e)));
+    console.log(Object.keys(json));
     // return jsonStr
     return {
         apiKey: json["apiKey"],
--- src/input-helper.ts
+++ src/input-helper.ts
@@ -37,6 +37,8 @@ function getFirebaseConfig(jsonStr): FireBaseConfigType {
   // if (jsonStr.trim() == "") core.setFailed("FireBase Config is Empty");
   const json = JSON.parse(JSON.stringify(jsonStr));
   console.log(json);
+  console.log(Object.keys(json).map((e)=> console.log(e)))
+  console.log(Object.keys(json))
   // return jsonStr
   return {
     apiKey: json["apiKey"],
